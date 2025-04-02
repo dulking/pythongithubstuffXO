58f1428602ba5cc9655bf38887691639207cfabb
dulking <cly0973@gmail.com>
2025-04-03 11:58:28 +1300

    The student info

--- practice_assignment.py
+++ practice_assignment.py
@@ -1,2 +1,5 @@
 # Simple activity registration with basic input/output
 print("Welcome to Karmu High School Activity Registration!")
+# Get student info
+name = input("Enter your name: ")
+age = input("Enter your age: ")
