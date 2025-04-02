2e976eee66bbe7a64dce51c6859c80a6040a2c8d
dulking <cly0973@gmail.com>
2025-04-03 12:00:19 +1300

    The activites that shows

--- practice_assignment.py
+++ practice_assignment.py
@@ -3,3 +3,8 @@ print("Welcome to Karmu High School Activity Registration!")
 # Get student info
 name = input("Enter your name: ")
 age = input("Enter your age: ")
+# Show activities
+print("\nChoose an activity:")
+print("1. Music Jam Session (2 hours, easy, $5)")
+print("2. Science Experiments Lab (3 hours, moderate, $10)")
+print("3. Sports Leadership Training (4 hours, challenging, $12)")
